ee3b85bfc999918cc2446e2c4d4ca464dd05e122
Jhon Rivero <jhonrivero21@gmail.com>
2025-02-23 23:56:17 -0500

    feat: integration with supabase - login

--- src/lib/utils/supabase.ts
+++ src/lib/utils/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SupabaseUrl = import.meta.env.SUPABASE_URL;
-const SupabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY;
+const SupabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
+const SupabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SupabaseUrl, SupabaseAnonKey)
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,25 @@
 <script>
-    import Header from '$lib/components/Header.svelte';
+    import { invalidate } from '$app/navigation'
+    import { onMount } from 'svelte'
     import '../app.css'
+    import Header from '$lib/components/Header.svelte';
     import { page } from '$app/state';
+
+    let { data, children } = $props()
+    let { session, supabase } = $derived(data)
+  
+    onMount(() => {
+      const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+        if (newSession?.expires_at !== session?.expires_at) {
+          invalidate('supabase:auth')
+        }
+      })
+  
+      return () => data.subscription.unsubscribe()
+    })
 </script>
 
 <Header redirection={page.url.pathname.includes('/private') ? '/home' : '/'} />
-<div class="max-w-screen-2xl mx-auto">
-    <slot />
+<div class="max-w-screen-2xl mx-auto"> 
+    {@render children()}
 </div>
