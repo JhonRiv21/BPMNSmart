ed59b7401f8aba2a124e2ab4fcc6cf530669fa17
JhonRiv21 <jhonrivero21@gmail.com>
2025-05-16 20:01:16 -0500

    feat: add id page bpmn

--- frontend/src/lib/components/Header.svelte
+++ frontend/src/lib/components/Header.svelte
@@ -4,9 +4,7 @@
 
 <header class="w-ful mx-auto px-5 py-3 shadow-md">
 	<div class="mx-auto flex max-w-screen-2xl flex-row items-center justify-between gap-4">
-		<div class="w-full">
-			<Logo />
-		</div>
+		<Logo />
 
 		<nav class="flex w-full max-w-max flex-row gap-6">
 			<a href="/login" class="button-principal">Ingresar</a>
