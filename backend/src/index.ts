04261c49e2146da8e17b09e641e8615a0d269a82
JhonRiv21 <jhonrivero21@gmail.com>
2025-06-03 03:09:36 -0500

    (back)/fix: expanded memory limit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,7 +23,8 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
